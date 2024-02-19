472d29179a39d94cc9e2344c874af8ca668c55d5
Roksana Parvin <maya.roksana@gmail.com>
2024-02-19 10:10:20 +0600

    Add files via upload

---
+++ config.py
@@ -0,0 +1,11 @@
+DATA_SOURCE = 'data/AMAZON_FASHION.parquet'
+TRAIN_DATA_SOURCE = 'data/AMAZON_FASHION_TRAIN.parquet'
+LENGTH = 5000
+VOCAB_SIZE = 1000
+BATCH_SIZE = 64
+ARTIFACT_STORE = 'artifacts'
+MODEL_PATH = 'artifacts/model.h5'
+TOKENIZER_PATH = 'artifacts/tokenizer.pkl'
+EPOCHS = 10
+STEPS_PER_EPOCH = 100
+EVALUATION_PATH='artifacts/metrics.csv'
